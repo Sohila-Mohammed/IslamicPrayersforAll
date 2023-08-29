af2a77a9097bf5e4da1e69b0c3001f1b3d530f3e
Sohila-Mohammed <142331981+Sohila-Mohammed@users.noreply.github.com>
2023-08-29 08:57:58 +0300

    Add files via upload

---
+++ wakingupprayers.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const btns = [...document.getElementsByClassName('add')],
+    btnsClicksCountArr = new Array(btns.length);
+    btns.forEach((el, i) => {
+        btnsClicksCountArr[i] = 0;
+        el.addEventListener('click', () => el.innerHTML = ++btnsClicksCountArr[i]);
+    });
+    document.querySelector("#all").onclick = function(){
+        one.innerHTML = "Alhamdu lillahil-ladhi 'ahyana ba`da ma 'amatana, wa 'ilayhin-nushur.";
+        two.innerHTML = "La 'ilaha 'illallahu wahdahu la sharika lahu, lahul-mulku wa lahul-hamdu, wa huwa 'ala kulli shay'in qadir Subhanallahi, walhamdu lillahi, wa la 'ilaha 'illallahu, wallahu 'akbar, wa la haula wa la quwwata 'illa billahil-`aliyyil-`azim, rabbighfir li.";
+        three.innerHTML = "Al-hamdu lillahil-ladhi `afani fi jasadi, wa radda `alayya ruhi, wa 'adhina li bidhikrihi.";
+    };
+    document.querySelector("#onee").onclick = function(){
+        one.innerHTML = "Alhamdu lillahil-ladhi 'ahyana ba`da ma 'amatana, wa 'ilayhin-nushur.";
+    };
+    document.querySelector("#twoo").onclick = function(){
+        two.innerHTML = "La 'ilaha 'illallahu wahdahu la sharika lahu, lahul-mulku wa lahul-hamdu, wa huwa 'ala kulli shay'in qadir Subhanallahi, walhamdu lillahi, wa la 'ilaha 'illallahu, wallahu 'akbar, wa la haula wa la quwwata 'illa billahil-`aliyyil-`azim, rabbighfir li.";
+    };
+    document.querySelector("#threee").onclick = function(){
+        three.innerHTML = "Al-hamdu lillahil-ladhi `afani fi jasadi, wa radda `alayya ruhi, wa 'adhina li bidhikrihi.";
+    };
+});
